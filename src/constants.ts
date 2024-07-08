cd3fcc442dc1c4f401975e0bfe164833ed8b75bc
cpcaballero <carlospcaballero@outlook.com>
2024-07-08 12:41:08 +0800

    Move constants to sepaarate file

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { CountryProps } from "./components/country/CountrySelect";
+
+export const DEFAULT_CURRENCY: string = "USD - US Dollar";
+
+export const DEFAULT_LANGUAGE = "English - English";
+
+export const DEFAULT_COUNTRY: CountryProps = {
+  code: "US",
+  name: "United States of America",
+};
